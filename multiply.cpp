58ae2ebe586442b5e1a6d098904644a668cd9714
Mohit Patil <mohitpatil2811@gmail.com>
2024-06-25 20:20:56 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+const ll mod=1000000007;
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define vec(v,a) vector<ll> v(a)
+#define sz(a) a.size()
+#define in(x) ll x; cin >> x;
+#define all(v) v.begin(),v.end()
+#define inp(v,b) for(ll i=0; i<b; ++i){cin>>v[i];}
+#define backrev(v) v.rbegin(),v.rend()
+#define f(i,a,b) for(ll i=a; i<b; ++i)
+#define rf(i,a,b) for(ll i = b-1;i>=a;i--)
+using namespace std;
+int main() {
+ll t;cin>>t;
+while(t--){
+in(a);in(b);
+ cout<<a*b<<endl;
+ 
+}
+   return 0;
+}
